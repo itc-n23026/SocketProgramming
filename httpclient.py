9d603fbcbdb60df7fe49a4e1c205cf4514e28597
itc-n23026 <n23026@std.it-college.ac.jp>
2023-10-31 07:19:05 +0000

    http client

---
+++ httpclient.py
@@ -0,0 +1,34 @@
+import socket
+
+def send_msg(sock, msg):
+    totalsl = 0
+    totalml = len(msg)
+    while totalsl < totalml:
+        sl = sock.send(msg[totalsl:])
+        if sl == 0:
+            raise RuntimeError('socket connection broken')
+        totalsl += sl
+
+def recv_msg(sock, cl=1024):
+    while True:
+        rc = sock.recv(cl)
+        if len(rc) == 0:
+            break
+        yield rc
+
+def main():
+    ip = input("IPアドレスを入力してください:")
+    port = int(input("ポート番号を入力してください:"))
+
+    cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cs.connect((ip, port))
+    reqt = 'GET / HTTP/1.0\r\n\r\n'
+    reqb = reqt.encode('ASCII')
+    send_msg(cs, reqb)
+    reqb = b''.join(recv_msg(cs))
+    reqt = reqb.decode('ASCII')
+    print(reqt)
+    cs.close()
+
+if __name__ == '__main__':
+    main()
